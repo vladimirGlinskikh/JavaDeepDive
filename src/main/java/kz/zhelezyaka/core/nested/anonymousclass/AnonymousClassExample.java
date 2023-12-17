f262b8a72015a409324c4acfabf05f2c90658e81
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-12-17 13:25:03 +0300

    Implementation of the Interface with Anonymous Class

---
+++ src/main/java/kz/zhelezyaka/core/nested/anonymousclass/AnonymousClassExample.java
@@ -0,0 +1,18 @@
+package kz.zhelezyaka.core.nested.anonymousclass;
+
+import static java.lang.System.*;
+
+interface Greeting {
+    void greet();
+}
+
+public class AnonymousClassExample {
+    public static void main(String[] args) {
+        // Использование анонимного класса для реализации интерфейса Greeting
+        Greeting anonymousGreeting =
+                () -> out.println("Calling from anonymous class!");
+
+        // Вызов метода greet() анонимного класса
+        anonymousGreeting.greet();
+    }
+}
