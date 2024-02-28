a178c7c462a2242f0aec821a8b7ebce3423ebbd9
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-02-28 17:47:31 +0300

    Check Done

---
+++ src/test/java/kz/zhelezyaka/leetcode/palindrome_number_9/Solution.java
@@ -0,0 +1,27 @@
+package kz.zhelezyaka.leetcode.palindrome_number_9;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+
+    public static boolean isPalindrome(int x) {
+        String stringValue = String.valueOf(x);
+        List<Character> list = new LinkedList<>();
+        for (char ch : stringValue.toCharArray()) {
+            list.add(ch);
+        }
+        ListIterator<Character> iterator = list.listIterator();
+        ListIterator<Character> reverseIterator = list.listIterator(list.size());
+        while (iterator.hasNext() && reverseIterator.hasPrevious()) {
+            if (iterator.next() != reverseIterator.previous()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
